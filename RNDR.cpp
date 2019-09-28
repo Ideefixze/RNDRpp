9b626fbf80573dbd5d5040af222e4d142265dd51
Ideefixze <44556744+Ideefixze@users.noreply.github.com>
2019-09-28 21:52:21 +0200

    Initial commit

---
+++ RNDR.cpp
@@ -0,0 +1,14 @@
+// RNDR.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Dupa" << std::endl;
+	int d;
+	std::cin >> d;
+    return 0;
+}
+
